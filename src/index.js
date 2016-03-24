3c706b722401c396bfc88b2fe2963972e5ee9abd
Montague <monty.monro@blackmilkclothing.com>
2016-03-24 14:20:40 +1000

    * Adding some browser detection * Adding MIT licence file

--- src/index.js
+++ src/index.js
@@ -7,10 +7,25 @@ import FCrypt from './fcrypt';
 
 const CSS_FILES = ['styles/font-awesome.min.css', 'styles/remodal.css', 'styles/remodal-default-theme.css'];
 const DOCK_WRAPPER = '.fbDockWrapper'
-let css = CSS_FILES.map(file => chrome.extension.getURL(file));
+let css;
 
-loadCSS(css);
 $('._li').addClass('remodal-bg');
+
+$(document).ready(function () {
+  const isChrome = navigator.userAgent.indexOf("Chrome") > -1;
+  const isFirefox = navigator.userAgent.indexOf("Firefox") > -1;
+  const isSafari = navigator.userAgent.indexOf("Safari") > -1;
+
+  if (isChrome) {
+    css = CSS_FILES.map(file => chrome.extension.getURL(file));
+  } else if (isFirefox) {
+    css = self.data.url(file);
+  } else if (isSafari) {
+    css = safari.extension.baseURI + file;
+  }
+
+  loadCSS(css);
+});
 
 $(window).load(function () {
   const CHATS = $('.fbNubFlyoutTitlebar.titlebar');
